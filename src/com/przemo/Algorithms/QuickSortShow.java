87fe1408d52d09b4fd0e157051f72fdf068c620b
przemyslaw.galera <przemodev>
2020-04-10 17:50:50 +0200

    QuickSort added

---
+++ src/com/przemo/Algorithms/QuickSortShow.java
@@ -0,0 +1,40 @@
+package com.przemo.Algorithms;
+
+import java.util.Arrays;
+
+public class QuickSortShow {
+    private static final int[] INPUT = new int[]{1, 3, 4, 5, 8, 6, 7, 2};
+
+    public void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; ++j) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int iElement = arr[i];
+        arr[i] = arr[j];
+        arr[j] = iElement;
+    }
+
+    public String getNotes() {
+        quickSort(INPUT, 0, INPUT.length - 1);
+        return Arrays.toString(INPUT);
+    }
+}
--- src/com/przemo/Main.java
+++ src/com/przemo/Main.java
@@ -1,13 +1,13 @@
 package com.przemo;
 
 import com.przemo.Algorithms.InsertionSortShow;
+import com.przemo.Algorithms.QuickSortShow;
 
 public class Main {
 
     public static void main(String[] args) {
-	    // Algorithms
-        System.out.println(new InsertionSortShow().getNotes());
-
-
+        // Algorithms
+        System.out.println("InsertionSort: " + new InsertionSortShow().getNotes());
+        System.out.println("QuickSort: " + new QuickSortShow().getNotes());
     }
 }
